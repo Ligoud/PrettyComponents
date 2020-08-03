2c7f4d0a873f148ad66412d735d610aef35c3b82
Yegor <ligoudnd@gmail.com>
2020-08-04 03:56:08 +0400

    firs com

---
+++ src/Components/Style1.js
@@ -0,0 +1,40 @@
+
+import React from 'react'
+
+import '../scss/style1.scss'
+
+class ButtonStyle1 extends React.Component{
+    constructor(props){
+        super(props)
+        this.handleClick=this.handleClick.bind(this)
+    }
+    handleClick(e){
+        if(this.props.onButtonClick!=undefined)
+            this.props.onButtonClick(e)
+        else
+            console.log('No handlers detected. Pass your handler from the parent component through the props.')
+    }
+    render(){
+        const fill=this.props.fill;
+        const stroke=this.props.stroke;
+        const text=this.props.text;
+
+        return(
+            <div className='buttonStyle1' >
+                <div onClick={this.handleClick}>
+                    <svg className='svg_style1'>
+                        <rect className='rect_style1'  
+                            strokeLinecap="round" 
+                            strokeLinejoin="round" 
+                            fill={fill?fill:'transparent'} 
+                            stroke={stroke?stroke:'rgba(153, 25, 153, 0.952)'}>                             
+                        </rect>
+                    </svg>
+                    <span className='span_style1'>{text?text:'Send'}</span>
+                </div>                
+            </div>
+        )
+    }
+}
+
+export default ButtonStyle1
--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from 'react-bootstrap/Navbar'
+//import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+import { Nav } from 'react-bootstrap'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './scss/index.scss'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import $ from 'jquery'
+
+//Custom components
+import ButtonStyle1 from './Components/Style1'
+//
+
+class Field extends React.Component {
+
+    switched(){
+      console.log('Something')
+      $('#field').toggleClass('dark')
+      
+    }
+    componentDidMount(){
+      console.log('Page loaded')
+    }
+    //
+
+    render() {    
+      return (
+        <div>
+          <Navbar  variant='dark' bg='dark' className='navtop'>
+            <Navbar.Brand href='#'>
+              <div className='cutlogo'>
+                <img
+                  src='/FirstLogo.png'
+                  alt=''
+                />  
+              </div>                                        
+            </Navbar.Brand>
+            <Nav className='ml-auto'>
+              <Form >
+                <Form.Switch
+                  inline
+                  custom
+                  id='themeSwitcher'
+                  label='On/Off dark theme'
+                  onChange={this.switched}
+                />
+              </Form>
+            </Nav>
+            
+          </Navbar>
+          
+          <div id='field'>
+            <figure>
+              <div className="card">
+                              
+                <ButtonStyle1></ButtonStyle1>
+              </div>
+              <figcaption>
+                <svg >
+                  <polygon points='0,0 200,0 170,40 30,40'   fill='transparent'></polygon>                  
+                </svg>
+                <span>Definition 1</span>
+              </figcaption>
+            </figure>
+            
+            <figure>
+              <div className="card">Box radius попробуй</div>
+              <figcaption>
+                <svg >
+                  <polygon points='0,0 200,0 170,40 30,40'   fill='transparent'/>
+                </svg>
+                <span>Definition 2</span>                
+              </figcaption>
+            </figure>
+          </div>        
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Field/>,
+    document.getElementById('root')
+  );
+  
