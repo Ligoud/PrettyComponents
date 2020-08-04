2a2001911c8ba7d8e175ee347e351750f339a069
Yegor <ligoudnd@gmail.com>
2020-08-04 04:02:50 +0400

    Updates

---
+++ precache-manifest.6b7e106e95efab317552981b63048d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396afe98264dc430803ea88481b31171",
+    "url": "/PrettyComponents/index.html"
+  },
+  {
+    "revision": "a5b67c0bafab708f83e9",
+    "url": "/PrettyComponents/static/css/main.07d083da.chunk.css"
+  },
+  {
+    "revision": "4f693137334687fd7206",
+    "url": "/PrettyComponents/static/js/2.a742d18c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/PrettyComponents/static/js/2.a742d18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b67c0bafab708f83e9",
+    "url": "/PrettyComponents/static/js/main.25c79087.chunk.js"
+  },
+  {
+    "revision": "70d978fabb9f87f8796e",
+    "url": "/PrettyComponents/static/js/runtime-main.1b09cc04.js"
+  }
+]);
