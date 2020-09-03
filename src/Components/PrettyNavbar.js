fc5e70ffa36814966d8010a3e23dc6e7d1d097b8
Yegor <ligoudnd@gmail.com>
2020-09-03 19:37:23 +0400

    navbar collapse fixedd

--- src/Components/PrettyNavbar.js
+++ src/Components/PrettyNavbar.js
@@ -1,55 +1,68 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Form from 'react-bootstrap/Form'
-import Nav  from 'react-bootstrap/Nav'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
 
-import $ from 'jquery'
+import $ from "jquery";
+import { NavDropdown } from "react-bootstrap";
 
-
-class PrettyNavbar extends React.Component{
-    constructor(props){
-        super(props)
-        this.switched=this.switched.bind(this)
-        this.handleClick=this.handleClick.bind(this)
-    }
-    switched(){
-        console.log('Something')
-        $('#field').toggleClass('dark')        
-    }
-    handleClick(e){
-        let link=e.target
-        let type=$(link).attr('data-type');
-        this.props.onNavClicked(type)
-    }
-    render(){
-        return(
-            <Navbar  variant='dark' bg='dark' className='navtop'>
-                <Navbar.Brand href='#'>
-                    <div className='cutlogo'>
-                        <img
-                        src={require('../FirstLogo.png')}
-                        alt=''
-                        />  
-                    </div>                                        
-                </Navbar.Brand>
-                <Nav className='mr-auto'>
-                    <Nav.Link onClick={this.handleClick} data-type='page1'>Pretty Components</Nav.Link>
-                    <Nav.Link onClick={this.handleClick} data-type='page2'>Pretty Sorting Algorithms</Nav.Link>
-                </Nav>
-                <Nav className='ml-auto'>
-                <Form >
-                    <Form.Switch
-                        inline
-                        custom
-                        id='themeSwitcher'
-                        label='On/Off dark theme'
-                        onChange={this.switched}
-                        />
-                    </Form>
-                </Nav>                    
-            </Navbar>            
-        )
-    }
+class PrettyNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.switched = this.switched.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  switched() {
+    console.log("Something");
+    $("#field").toggleClass("dark");
+  }
+  handleClick(e) {
+    let link = e.target;
+    let type = $(link).attr("data-type");
+    this.props.onNavClicked(type);
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          variant="dark"
+          bg="dark"
+          className="navtop"
+        >
+          <Navbar.Brand href="#">
+            <div className="cutlogo">
+              <img src={require("../FirstLogo.png")} alt="" />
+            </div>
+            {/* sssxxx */}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link onClick={this.handleClick} data-type="page1">
+                Pretty Components
+              </Nav.Link>
+              <Nav.Link onClick={this.handleClick} data-type="page2">
+                Pretty Sorting Algorithms
+              </Nav.Link>
+            </Nav>
+            <Nav className="ml-auto">
+              <Form>
+                <Form.Switch
+                  inline
+                  custom
+                  id="themeSwitcher"
+                  label="On/Off dark theme"
+                  onChange={this.switched}
+                />
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
 
-export default PrettyNavbar
+export default PrettyNavbar;
--- src/index.js
+++ src/index.js
@@ -1,57 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-import './scss/index.scss'
-
+import "./scss/index.scss";
 
 //Pages
-import PrettyComponents from './Components/PrettyComponents'
-import PrettyNavbar from './Components/PrettyNavbar'
-import PrettySortingAlgs from './Components/PrettySortingAlgs'
+import PrettyComponents from "./Components/PrettyComponents";
+import PrettyNavbar from "./Components/PrettyNavbar";
+import PrettySortingAlgs from "./Components/PrettySortingAlgs";
 
 //
 
 class Field extends React.Component {
-    constructor(props){
-      super(props)
-      this.navclicked=this.navclicked.bind(this)
-      this.state={
-        navPage:'page1',
-      }
-    }
-    navclicked(type){
-      this.setState({navPage:type})
-
-    }
-    componentDidMount(){
-      console.log('Page loaded')
-    }
-    //
-
-    render() {        
-      let body
-      if(this.state.navPage==='page1'){
-        body=<PrettyComponents/> 
-      }else if(this.state.navPage==='page2'){
-        body=<PrettySortingAlgs key={new Date()}/>  //Чтобы постоянно работала кнопка
-      }  
-
-      return (
-        <div>
-          <PrettyNavbar onNavClicked={this.navclicked}/>
-          <div id='field' className='hideScrollBar'> 
-            {body}           
-          </div>          
-        </div>        
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.navclicked = this.navclicked.bind(this);
+    this.state = {
+      navPage: "page1",
+    };
   }
-  // <PrettyComponents/> 
-  // ========================================
-  
-  ReactDOM.render(
-    <Field/>,
-    document.getElementById('root')
-  );
-  
+  navclicked(type) {
+    this.setState({ navPage: type });
+  }
+  componentDidMount() {
+    console.log("Page loaded");
+  }
+  //
+
+  render() {
+    let body;
+    if (this.state.navPage === "page1") {
+      body = <PrettyComponents />;
+    } else if (this.state.navPage === "page2") {
+      body = <PrettySortingAlgs key={new Date()} />; //Чтобы постоянно работала кнопка
+    }
+
+    return (
+      <div>
+        <PrettyNavbar onNavClicked={this.navclicked} />
+        <div id="field" className="hideScrollBar">
+          {body}
+        </div>
+      </div>
+    );
+  }
+}
+// <PrettyComponents/>
+// ========================================
+
+ReactDOM.render(<Field />, document.getElementById("root"));
